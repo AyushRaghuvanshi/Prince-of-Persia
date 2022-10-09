d11c42ea22e6f36c41d8ec3fff7afa33b4a74a91
Ayush <ayushraghuvanshi03@gmail.com>
2022-10-09 18:25:26 +0530

    Fix Not Being Able to Swtich Screens

--- animation.js
+++ animation.js
@@ -11,32 +11,35 @@ backgrounds.push(new Image());
 // let b = new Image();
 backgrounds[0].src = "Backgrounds/1.jpg";
 backgrounds[1].src = "Backgrounds/2.jpg";
+console.log(backgrounds);
 let playerImage = new Image();
 playerImage.src = "Player/player1.png";
 
-function animate(){
-    // console.log(player.x, canvasWidth);
-    
-    ctx.clearRect(0,0, canvasWidth, canvasHeight);
-    ctx.drawImage(backgrounds[screenNumber-1], 0, 0, canvasWidth, canvasHeight);
-    ctx.drawImage(playerImage, player.x, player.y-player.height, 100, 100);
-    checkScreen();
-    console.log(screenNumber, player.x);
-    requestAnimationFrame(animate);
+function animate() {
+  // console.log(player.x, canvasWidth);
+
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+  ctx.drawImage(backgrounds[screenNumber - 1], 0, 0, canvasWidth, canvasHeight);
+  ctx.drawImage(playerImage, player.x, player.y - player.height, 100, 100);
+  checkScreen();
+  console.log(player.x, canvasWidth, screenNumber);
+  requestAnimationFrame(animate);
 }
 
-function checkScreen(){
-    if(player.x + player.width >= canvasWidth){
-        screenNumber++;
-        clearInterval(gravtity);
-        player.x = 0;
-        player.y = 0;
-        gravtity = setInterval(movements, 10);
-    }
-    if(player.x <= 0){
-        screen--;
-        player.x = canvasWidth - player.width;
-    }
+function checkScreen() {
+  if (player.x + player.width >= canvasWidth) {
+    screenNumber++;
+    clearInterval(gravtity);
+    player.x = 5;
+    player.y = 0;
+
+    gravtity = setInterval(movements, 10);
+  } else if (player.x <= 0) {
+    console.log(screenNumber);
+    screenNumber--;
+    console.log(screenNumber);
+    player.x = canvasWidth - player.width - 5;
+  }
 }
 
 animate();
